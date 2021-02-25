e3e2e02df9e8bbd2faeee9056115b38a275db641
Aleksei Yarantsev <aleksei.yarantsev@gmail.com>
2021-02-25 11:28:20 -0800

    fix commit issue

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
-// var createError = require("http-errors");
 const exppress = require("express");
-// const bodyParser = require("body-parser");
-const https = require("https"); //build in module
+const bodyParser = require("body-parser");
 
 const app = exppress();
 
@@ -31,19 +29,3 @@ app.get("/", function (req, res) {
 app.listen(3000, function () {
   console.log("Server started on port 3000!!");
 });
-
-// catch 404 and forward to error handler
-// app.use(function (req, res, next) {
-//   next(createError(404));
-// });
-
-// // error handler
-// app.use(function (err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get("env") === "development" ? err : {};
-
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render("error");
-// });
