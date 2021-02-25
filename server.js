43df2e80259d8b7f55a16ce18ceca47d9c69cdcb
Aleksei Yarantsev <aleksei.yarantsev@gmail.com>
2021-02-25 11:21:47 -0800

    update repo

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = exppress();
 
 app.get("/", function (req, res) {
   const url =
-    "https://api.openweathermap.org/data/2.5/weather?q=Irvine&units=imperial&appid=" +
+    "https://api.openweathermap.org/data/2.5/weather?q=Irvine&appid=" +
     process.env.WEATHER_API_KEY;
   https.get(url, function (response) {
     //checking the response from API call
@@ -16,6 +16,8 @@ app.get("/", function (req, res) {
     //getting weather data from API
     response.on("data", function (data) {
       const weatherData = JSON.parse(data);
+      console.log("weatherData", weatherData);
+
       const temparautre = weatherData.main.temp;
       console.log("this is the temparautre", temparautre);
       const description = weatherData.weather[0].description;
