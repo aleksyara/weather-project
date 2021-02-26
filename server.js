ef73b54aacd11791b028d83d1a6de698ecd4b808
Aleksei Yarantsev <aleksei.yarantsev@gmail.com>
2021-02-26 12:44:38 -0800

    fix weather API, render current temp, wind and icon

--- server.js
+++ server.js
@@ -1,29 +1,52 @@
+require("dotenv").config();
+const e = require("express");
 const exppress = require("express");
-const bodyParser = require("body-parser");
+// const bodyParser = require("body-parser");
+const https = require("https");
 
 const app = exppress();
 
+const key = process.env.WEATHER_API_KEY;
+
 app.get("/", function (req, res) {
   const url =
-    "https://api.openweathermap.org/data/2.5/weather?q=Irvine&appid=" +
-    process.env.WEATHER_API_KEY;
-  https.get(url, function (response) {
-    //checking the response from API call
-    console.log("this is reponse", response.statusCode);
+    "https://api.openweathermap.org/data/2.5/weather?q=Irvine&units=imperial&appid=" +
+    key;
 
-    //getting weather data from API
-    response.on("data", function (data) {
-      const weatherData = JSON.parse(data);
-      console.log("weatherData", weatherData);
+  https
+    .get(url, function (response) {
+      //checking the response from API call
+      // console.log("statusCode:", response.statusCode);
 
-      const temparautre = weatherData.main.temp;
-      console.log("this is the temparautre", temparautre);
-      const description = weatherData.weather[0].description;
-      console.log("description -->>>", description);
+      //getting weather data from API
+      response.on("data", function (data) {
+        const weatherData = JSON.parse(data);
+        console.log("weatherData", weatherData);
+
+        const temparautre = weatherData.main.temp;
+        const description = weatherData.weather[0].description;
+        const windSpeed = weatherData.wind.speed;
+        const icon = weatherData.weather[0].icon;
+        const imgUrl = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+        res.write(
+          "<p> The weather in Irvine describes as " + description + "<p>"
+        );
+        res.write(
+          "<h1>Temparautre in Irvine is " +
+            temparautre +
+            "F. And the wind speed is " +
+            windSpeed +
+            "mph. </h1>"
+        );
+        res.write("<img src=" + imgUrl + ">");
+        res.send();
+      });
+    })
+    .on("error", (e) => {
+      console.log("error", e);
     });
-  });
 
-  res.sendFile(__dirname + "/index.html");
+  // res.sendFile(__dirname + "/index.html");
 });
 
 app.listen(3000, function () {
