7e141c9bf003c3ab15b21a2c995f97d007a9fb2c
Aleksei Yarantsev <aleksei.yarantsev@gmail.com>
2021-03-03 17:19:15 -0800

    code refactoring, move to ejs

---
+++ data/weather-db.js
@@ -0,0 +1,9 @@
+const myWeatherDatas = [
+    {userInput: "Norilsk", temperature: 0, windSpeed: 0, }
+];
+
+module.exports = {
+    getAll: function() {
+      return myWeatherDatas;
+    }
+  };
--- server.js
+++ server.js
@@ -3,24 +3,42 @@ require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const https = require("https");
+const path = require("path");
+// require the todo "database"
+const weatherDb = require("./data/weather-db");
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true })); // use for getting data from req.body
+app.use(express.static(__dirname + "/public"));
 
-const key = process.env.WEATHER_API_KEY; //key autherntication
-app.use(express.static(__dirname + '/public'));
+const key = process.env.WEATHER_API_KEY; //key authentication
 
+//Set initital array for the Todo items
+let temperatures = [0];
+let userInput = "X";
+
+// Configure the app (app.set)
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+//Routing
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + "/index.html");
+  res.render("index");
 });
 
+app.get("/city", function (req, res) {
+  res.render("city", { temperatures: temperatures, userInput: userInput });
+  // res.render("city", {
+  //   items: weatherDb.getAll(),
+  // });
+});
 
 // Landing Main Page
 
 // Catch user input and render weather data
 app.post("/", function (req, res) {
-  const userInput = req.body.cityName;
+  userInput = req.body.cityName;
   const units = "imperial";
   const url =
     "https://api.openweathermap.org/data/2.5/weather?q=" +
@@ -40,30 +58,32 @@ app.post("/", function (req, res) {
       response.on("data", function (data) {
         //parse data from 3rd party API
         const weatherData = JSON.parse(data);
-        const temparautre = weatherData.main.temp;
+        let temperature = weatherData.main.temp;
+        temperatures.push(temperature);
         const description = weatherData.weather[0].description;
         const windSpeed = weatherData.wind.speed;
         const icon = weatherData.weather[0].icon;
         const imgUrl = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
         // res.write("<p> The weather is currently " + description + "<p>");
         // res.write(
-        //   "<h1>The temparautre in " +
+        //   "<h1>The temperature in " +
         //     userInput +
         //     " is " +
-        //     temparautre +
+        //     temperature +
         //     "F. And the wind speed is " +
         //     windSpeed +
         //     "mph. </h1>"
         // );
         // res.write("<img src=" + imgUrl + ">");
         // res.send();
-        res.sendFile(__dirname + "/views/city.html");
+        // res.render(__dirname + "/views/city.html");
       });
     })
-
     .on("error", (e) => {
       console.log("error", e);
     });
+
+  res.render("city", { temperatures: temperatures, userInput: userInput });
 });
 
 app.listen(3000, function () {
